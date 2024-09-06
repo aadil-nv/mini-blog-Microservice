7e8678a3857990eb1f365954015e37c678919e66
aadil nv <adilev2000@gmail.com>
2024-09-06 20:37:34 +0530

    2 commit

--- backend/userService/server.js
+++ backend/userService/server.js
@@ -7,7 +7,7 @@ const colors = require('colors')
 const userRoutes = require('./Routes/userRouter')
 const connectDB = require('./config/db')
 
-// jhgjghgjssssassahghjgjgjhgdsfsdfsdfsdfsdfsdfdsdhdhdh
+// jhgjghdfsdfsdfsdfgjssssassahghjgjgjhgdsfsdfsdfsdfsdfsdfdsdhdhdh
 app.use(express.json())
 connectDB()
 
